d42a45e9c712b475db29a463f4b3d42f5fbb8da0
VoltrefferNL <nielsdevisser@hotmail.com>
2020-07-01 19:24:20 +0100

    Added componentDidmount and state in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+import * as api from "./api";
+
+class App extends React.Component {
+  state = {
+    bestTenAirQuality: [],
+    worstTenAirQuality: [],
+  };
+
+  async componentDidMount() {
+    const worstTenAirQuality = await getWorstAirQuality("GB");
+    const bestTenAirQuality = await getBestAirQuality("GB");
+
+    this.setState({ worstTenAirQuality, bestTenAirQuality });
+  }
+
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
